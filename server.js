f40bf1fd0bc153f0233356847d94d73a5529e84d
Rituraj <riturajgolu01@gmail.com>
2021-07-12 11:03:49 +0530

    initialise this app

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express();
+const ejs = require('ejs');
+const expressLayout = require("express-ejs-layouts");
+const path = require('path')
+
+
+const port = process.env.port || 5000;
+
+app.get('/', (req, res) => {
+    res.render("home")
+})
+
+// set templates
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/templates/views'))
+app.set('view engine', 'ejs')
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`server is running at port no ${port}`)
+})
